5056354291306dfcda215012529c22e4ec0f571c
Leah <leahpwatt@gmail.com>
2016-05-12 17:09:45 -0400

    comments

--- selectController.js
+++ selectController.js
@@ -1,10 +1,10 @@
 var app = angular.module("reddit");
 
 app.controller("selectController", function($scope, redditService, $location){
-	$scope.getPage = function(getAww){
-	redditService.chooseSubReddit(getAww);
-	// console.log(getAww);
-	$location.path("/display");
+	$scope.getPage = function(){//getPage is triggered on the click of the button
+		//$scope.getPage has a vlue of the input(getAww)
+		redditService.chooseSubReddit($scope.getAww); // chooseSubReddit created on service.js
+		$location.path("/display");
 	};
 
 });
--- service.js
+++ service.js
@@ -1,15 +1,18 @@
-var app = angular.module("reddit");
+var app = angular.module("reddit"); //referencing a module, already created, called reddit
 
-app.factory("redditService", function($http){
-var subReddit = "";
-	function getReddit(){
-		console.log(subReddit);
-		return $http.get("https://www.reddit.com/r/"+subReddit+"/.json");
+app.factory("redditService", function($http){ //creating a service (factory is function); $http is another service that is part of angular framework
+
+	var subReddit = ""; //creating var with empty string
+	function getReddit(){ //creating function called getReddit
+		return $http.get("https://www.reddit.com/r/"+subReddit+"/.json"); //get data that is at json link and returns it; subReddit refers to var in line 5
+		//this function is returning an object/json 
 	}
 
 
 	return {
 		chooseSubReddit: function(reddit){
+			//choose SubReddit function is created here
+			//whatever is passed is the vlue of subReddit
 			subReddit = reddit;
 		},
 
